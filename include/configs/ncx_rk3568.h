366866ad8dbc8dbe3ed486905f6dbbbac29d971b
zcit <zcit123@address.com>
2022-08-24 16:11:52 +0400

    Test:extraenv 7

--- include/configs/ncx_rk3568.h
+++ include/configs/ncx_rk3568.h
@@ -25,10 +25,10 @@
 		"bank=b\0" \
         "bank_select_files="  \
         "if  test ${bank} = a; then " \
-                setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rw rootfstype=ext4 rootwait atmel.pm_modes=standby,ulp0; setenv kernel_boot kernel_a; setenv itb_boot itb_a;" \
+               "setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rw rootfstype=ext4 rootwait atmel.pm_modes=standby,ulp0; setenv kernel_boot kernel_a; setenv itb_boot itb_a;" \
         "else" \
                "echo Booting Bank B;"	      \
-        "fi; \0" \
+        "fi; \0" 
 
 
 #undef CONFIG_BOOTCOMMAND
