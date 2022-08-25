4f670b1ffc10f20b96bede86163056f082ffefd0
zcit <zcit123@address.com>
2022-08-25 19:17:07 +0400

    change boot addr

--- include/configs/ncx_rk3568.h
+++ include/configs/ncx_rk3568.h
@@ -62,6 +62,17 @@
 	"rootfs partubi rootfs;" \
 	"userdata partubi userdata\0"
 
+
+#define NCX_SLOT_SETTINGS \
+        "bankz_sel=" \
+        "if test ${bank} = a; then " \
+				"setenv bootargs \"storagemedia=emmc androidboot.storagemedia=emmc androidboot.mode=normal root=/dev/mmcblk0p3 rw rootwait ; \" " \
+                "echo Boot from Bank A;" \
+       "elif test ${bank} = b; then " \
+                "echo Boot from Bank B;" \
+				"setenv bootargs \"storagemedia=emmc androidboot.storagemedia=emmc androidboot.mode=normal root=/dev/mmcblk0p6 rw rootwait ; \" " \
+        "fi; \0"
+		
 #endif /* CONFIG_ANDROID_AB */
 #endif /* CONFIG_SPL_BUILD */
 #endif /* __CONFIGS_RK3568_EVB_H */
