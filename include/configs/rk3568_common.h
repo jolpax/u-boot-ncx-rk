9ce091a272823ed60f4cf2e5e7df4ee9ae187ad5
zcit <zcit123@address.com>
2022-08-26 18:03:25 +0400

    chnaged6 :slot1-3

--- include/configs/rk3568_common.h
+++ include/configs/rk3568_common.h
@@ -84,12 +84,9 @@
 	"if test ${slot} = a; then " \
 			"echo Boot from slot A;" \
 			"setenv bootargs  storagemedia=emmc androidboot.storagemedia=emmc androidboot.mode=normal root=/dev/mmcblk0p3 rw rootwait ; " \
-   "elif test ${slot} = b; then " \
+	"elif test ${slot} = b; then " \
 			"echo Boot from slot B;" \
 			"setenv bootargs storagemedia=emmc androidboot.storagemedia=emmc androidboot.mode=normal root=/dev/mmcblk0p4 rw rootwait ;  " \
-	"elif test ${slot} = \0; then " \
-	"echo Boot from slot default A;" \
-	"setenv bootargs storagemedia=emmc androidboot.storagemedia=emmc androidboot.mode=normal root=/dev/mmcblk0p3 rw rootwait ;  " \
 	"fi; \0"
 
 #include <config_distro_bootcmd.h>
