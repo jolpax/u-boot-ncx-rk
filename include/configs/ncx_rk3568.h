9a4a88df64b3b06f3584e5832687f0f4a41ed7a9
zcit <zcit123@address.com>
2022-09-27 17:56:36 +0400

    test :added env to part 3 & changed partitions accordingly

--- include/configs/ncx_rk3568.h
+++ include/configs/ncx_rk3568.h
@@ -12,8 +12,6 @@
 #define CONFIG_SYS_MMC_ENV_DEV		0
 #undef CONFIG_ENV_OFFSET
 #undef CONFIG_ENV_SIZE
-//#define CONFIG_ENV_OFFSET                    0x003f8000
-//#define CONFIG_ENV_SIZE                         0x40000
 
 #define CONFIG_ENV_OFFSET                       0x000000
 #define CONFIG_ENV_SIZE                         0x40000
